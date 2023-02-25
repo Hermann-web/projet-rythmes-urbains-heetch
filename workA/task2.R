a74823917373b4c5395af0942c6b575ea6e652cb
Hermann-web <69398651+Hermann-web@users.noreply.github.com>
2023-02-25 15:43:59 +0100

    concatenate files

--- workA/task2.R
+++ workA/task2.R
@@ -1,6 +1,7 @@
 ############### Project #################
+# Intro
 
-# 1. Load packages ------
+## 1. Load packages ------
 
 library(dplyr) 
 library(sf)
@@ -9,25 +10,26 @@ library(lubridate)
 library(mapsf)
 library(tidyr)
 
-# 2. Load data -------
+## 2. Load data -------
 
 heetchPoints <-readRDS("DATA/heetchmarchcrop.Rds")
 casaNeib <-st_read("DATA/casaneib.geojson") 
 
-# 3. Projeter les objets spatiaux ------
+## 3. Projeter les objets spatiaux ------
 
 casaNeibProj <-st_transform(casaNeib, crs=26191)
 heetchPointsProj <-st_transform(heetchPoints, crs=26191)
 
-# 4. Créer la section du 1er Mars ------
+## 4. Créer la section du 1er Mars ------
 
 heetchPointsProj$Day <-day(heetchPointsProj$location_at_local_time)
 heetchPointsProj$Hour <-hour(heetchPointsProj$location_at_local_time)
 heetchM1Proj <-heetchPointsProj %>% 
-  filter(Day==1) %>% 
-  filter(Hour<23 & Hour>6 )
+  filter(Day==1) %>%  ##for all tasks
+  #filter(Hour<23 & Hour>6 ) %>%  ##only for task1
+  #filter(Hour>22 | Hour<7 ) ##only for task2
 
-# 5. Intersecter les points x et les quartiers y ------
+## 5. Intersecter les points x et les quartiers y ------
 
 casaNeibUnion <-st_union(casaNeibProj)
 ptsInCasa <-st_contains(x=casaNeibUnion, y=heetchM1Proj)
@@ -39,23 +41,38 @@ ptsInNeib <-st_within(x=heetchM1Proj, y=casaNeibProj) %>%
   unlist() 
 heetchM1CropProj$NEIB <-ptsInNeib
 
-# 6. Grouper par heure/ par identifiant chauffeur et par quartier --------
+## 6. Grouper par heure/ par identifiant chauffeur et par quartier --------
 
 nestedPts <-heetchM1CropProj %>% 
   st_drop_geometry() %>% 
   group_by(Hour, NEIB, driver_id) %>% 
   summarize(NBPTS=n())
 
-# 7. Selectionner le quartier principal pour chaque chauffeur à chaque heure
+# task 2
+## 7. Selectionner le quartier principal pour chaque chauffeur à chaque heure
 
 mainNeib <-nestedPts %>% 
   group_by(driver_id, Hour) %>% 
   arrange(desc(NBPTS)) %>% 
   slice(1)
 
-# 8. Pour chaque heure, pour chaque quartier, calculer le nombre de chauffeurs
+## 8. Pour chaque heure, pour chaque quartier, calculer le nombre de chauffeurs
 
 mainNeibJ <-nestedPts %>% 
   group_by(Hour, NEIB) %>% 
   summarize(NBDRiver=n())
 
+# task 4
+## 6. Grouper par quartier et par heure  --------
+
+nestedPts2 <-heetchM1CropProj %>% 
+  st_drop_geometry() %>% 
+  group_by(NEIB,Hour) %>% 
+  summarize(NBPTS=n())
+
+## 7. Selectionner pour chaque quartier l'heure qui contient plus de points
+
+mainHour2 <-nestedPts2 %>% 
+  group_by(NEIB) %>% 
+  arrange(desc(NBPTS)) %>% 
+  slice(1)
